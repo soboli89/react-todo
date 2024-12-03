6b4f2b2b54f0955a87062b7f372bb0dc7941bc09
soboli89 <84281763+soboli89@users.noreply.github.com>
2024-12-02 21:10:08 -0600

    added input function

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -3,15 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-
-function AddTodoForm (){
+function AddTodoForm ({ onAddTodo }){
+    const [newTodoTitle, setNewTodo] = useState('');
+    const handleAddTodo = (e) =>{
+        e.preventDefault();
+        onAddTodo({id: Date.now(), name: newTodoTitle});
+        setNewTodo('');
+    }
+    const handleChange =(e) => {
+        const setNewTodoItem = e.target.value;
+        setNewTodo(setNewTodoItem);
+    }
     return(
-            <form>
-                <label htmlFor="todoTitle">Title</label><br/>
-                <input id="todoTitle" type="text"></input><br/>
-                <input type="submit"></input>
-            </form>
+        <form onSubmit={handleAddTodo}>
+            <label htmlFor="todoTitle" >Title</label><br/>
+            <input 
+                name="item" 
+                id="todoTitle" 
+                type="text" 
+                value={newTodoTitle}
+                onChange={handleChange}
+            />
+            <br/>
+            <input type="submit" ></input>
+        </form>
     )
 };
 
---
+++ src/TodoListItem.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function TodoListItem (props) {
+    return (
+        <li>{props.item.name}</li>
+    )
+}
+
+
+export default TodoListItem;
