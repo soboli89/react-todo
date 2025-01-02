a85155fa38bd68651ddacc74397d52ecf4e4a357
soboli89 <84281763+soboli89@users.noreply.github.com>
2025-01-01 19:49:16 -0600

    added pormise

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
-function useSemiPersistentState () {
+
+/*function useSemiPersistentState () {
   const [todoList, setList] = useState(() => {
       const savedTodoList = JSON.parse(localStorage.getItem('todoList'));
     return savedTodoList ? savedTodoList : [];
@@ -14,14 +15,42 @@ function useSemiPersistentState () {
   useEffect(()=> localStorage.setItem('todoList', JSON.stringify(todoList)),[todoList]); 
   return [todoList, setList];
 }
+*/
 
 function App() {
+  
   const handleRemove = (item) => {
     const newList=todoList.filter((todoItem)=> item.id !== todoItem.id);
     setList(newList);
   }
+const [isLoading, setisLoading] = useState(true);
+const [todoList, setList] = useState(() => {
+      const savedTodoList = JSON.parse(localStorage.getItem('todoList'));
+    return savedTodoList ? savedTodoList : [];
+  });
 
-  const [todoList, setList] = useSemiPersistentState();
+useEffect(() => {
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          todoList: JSON.parse(localStorage.getItem('todoList')) || []}
+      })}, 2000) }
+      ).then((result) => {
+        setList(result.data.todoList);
+        setisLoading(false);
+      })
+}, []);
+
+ useEffect(() => {
+  if(!isLoading) {
+    localStorage.setItem('todoList', JSON.stringify(todoList)),[todoList]
+  } 
+});
+
+
+
+ // const [todoList, setList] = useSemiPersistentState();
  
   const handleAddTodo = (newTodo) => {
     setList([newTodo, ...todoList]);
@@ -30,7 +59,12 @@ function App() {
     <div>
       <h1>Todo List</h1>
         <AddTodoForm addTodo={handleAddTodo}/>
-        <TodoList todoList={todoList} onRemove={handleRemove}/>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <TodoList todoList={todoList} onRemove={handleRemove}/>
+        )}
+        
       </div>
   )
 }
