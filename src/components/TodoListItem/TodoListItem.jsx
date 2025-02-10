5926fa45ddf1907ba22d2c3942ad8f455e17557e
soboli89 <84281763+soboli89@users.noreply.github.com>
2025-02-09 18:57:23 -0600

    added PropTypes

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,21 +1,25 @@
-import Button from '../Button'
+import Button from '../../Button'
 import style from './TodoListItem.module.css'
-
+import PropTypes from "prop-types";
 
 
 function TodoListItem ({item, onRemove}) {
-    return (
-        
+    return (        
         <li className={style.listItem}>
             <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 11.917 9.724 16.5 19 7.5"/>
             </svg>
-
             {item.title}
             <Button label="Delete" onClick={()=>onRemove(item)}></Button>
         </li>
     )
 }
+
+TodoListItem.propTypes = {
+    todoLiitemst: PropTypes.object,
+    onRemove: PropTypes.func,
+  };
+
 
 export default TodoListItem;
 
